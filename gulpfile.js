75608eb488937f402ae785ce0fca5451b1377055
Aditya Wiraha Hadi Saputra <squalvj@Adityas-MacBook-Pro.local>
2018-09-16 01:38:01 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var stylus = require('gulp-stylus');
+var minifyCSS = require('gulp-csso');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('html', function(){
+  return gulp.src('src/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('dist/html'))
+});
+
+gulp.task('css', function(){
+  return gulp.src('assets/css/*.styl')
+    .pipe(stylus())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist/html/css'))
+});
+
+gulp.task('js', function(){
+  return gulp.src('assets/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/html/js'))
+});
+
+// gulp.task('default', function(){
+//    gulp.watch([ 'html', 'css', 'js' ])
+// });
+
+gulp.task('default', function () {
+   gulp.watch('assets/css/*.styl', ['css']);
+   gulp.watch('assets/js/*.js', ['js']);
+   gulp.watch('src/*.pug', ['html']);
+   gulp.watch('src/*/*.pug', ['html']);
+});
